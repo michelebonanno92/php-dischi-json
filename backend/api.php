4f98a9a0a93a495b9c2f7870ade72555d5393368
micheleboanno92 <michelebonanno92@gmail.com>
2024-10-01 17:46:56 +0200

    commentato codice

--- backend/api.php
+++ backend/api.php
@@ -3,9 +3,9 @@
 /* PER IL FUTURO */
 // header("Access-Control-Allow-Origin: http://127.0.0.1:5173");
 // header("Access-Control-Allow-Headers: X-Requested-With");
-	
+	// con file_get_contents prendiamo il contenuto di un file
 $studentsString = file_get_contents('db/students.json');
- 
+//  con decode lo decodifichiamo 
 $studentsArr = json_decode($studentsString, true);
 
 $students = [];
